05b125f1ceb1077238e987ef7bac34755a3a4a7b
SantiagoVillarroel <santigibsonsg@gmail.com>
2022-05-10 14:08:34 -0300

    commit inicial

---
+++ src/Components/Cell.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+const Cell = () => {
+  const [color, setColor] = useState(0)
+
+  return (
+    <div className="cell">
+      
+    </div>
+  )
+}
+
+export default Cell
---
+++ src/Components/Grid.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Cell from './Cell';
+
+const Grid = () => {
+  function renderCells() {
+    
+  }
+
+  return (
+    <div className="grid">
+      <Cell/>
+      <Cell/>
+      <Cell/>
+      <Cell/>
+    </div>
+  )
+}
+
+export default Grid
