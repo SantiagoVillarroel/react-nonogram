ad92ed420d24397eec26d1830feadf7c27e9fd06
SantiagoVillarroel <santigibsonsg@gmail.com>
2022-05-10 14:40:44 -0300

    basic cell color change

--- src/Components/Cell.js
+++ src/Components/Cell.js
@@ -3,10 +3,14 @@ import React, {useState} from 'react'
 const Cell = () => {
   const [color, setColor] = useState(0)
 
+  function changeColor(){
+    setColor((color+1) % 2)
+  }
+
   return (
-    <div className="cell">
+    <button className="cell" onClick = {() => changeColor()} style = {{backgroundColor: color===0 ? "white" : "black"}}>
       
-    </div>
+    </button>
   )
 }
 
